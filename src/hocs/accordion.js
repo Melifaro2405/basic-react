16d0500e3e2b97e5b5ce0ec9cff59fde52d47f60
Denis Lantsov <tomelifaro@yandex.ru>
2021-01-08 22:52:34 +0300

    add accordion decorator

---
+++ src/hocs/accordion.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default (OriginalComponent) => class DecoratedComponent extends Component {
+
+  state = {
+    openItemId: null
+  }
+
+  toggleOpenItem = (openItemId) => this.setState({openItemId})
+
+  render() {
+    return <OriginalComponent
+      {...this.props}
+      {...this.state}
+      toggleOpenItem={this.toggleOpenItem}
+      />
+  }
+}
