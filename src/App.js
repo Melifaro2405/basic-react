64932b0a07d0cb3ebe7a6e798346d8786b59b64e
Denis Lantsov <tomelifaro@yandex.ru>
2021-01-09 01:06:39 +0300

    add react-select

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react'
+import Select from 'react-select'
+
 import ArticleList from './components/article-list'
 import articles from './fixtures'
 
 class App extends Component {
+
+  state = {
+    openItem: null
+  }
+
   render() {
     return (
       <div>
+        <Select options={this.options} value={this.state.openItem} onChange={this.handleSelect} />
         <ArticleList articles={articles} />
       </div>
     )
+  }
+
+  handleSelect = (openItem) => this.setState({openItem})
+
+  get options() {
+    return articles.map((article) => ({
+      label: article.title,
+      value: article.id
+    }))
   }
 }
 
--- src/hocs/accordion.js
+++ src/hocs/accordion.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (OriginalComponent) => class DecoratedComponent extends Component {
 
   state = {
